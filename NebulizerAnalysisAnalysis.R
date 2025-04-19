617a26b0ad342a79e57ee8b89bb34ea20468e36c
TheGiraffe <TheGiraffe@users.noreply.github.com>
2025-04-18 21:19:16 -1000

    Flow rate conversions and test plot animation

--- NebulizerAnalysisAnalysis.R
+++ NebulizerAnalysisAnalysis.R
@@ -1,12 +1,15 @@
 library(tidyverse)
 library(ggplot2)
 library(gganimate)
+library(extrafont)
+font_import()
+loadfonts(device = "win")
 
 rm(list = ls())
 
 dataPath = "Data/ExportData"
-dataFolder ="Exp033-automated"
-filename = "Exp33-1001.txt"
+dataFolder ="Exp020-automated"
+filename = "Exp20-1001.txt"
 fullPath = paste(dataPath, dataFolder, filename, sep="/")
 
 data <- read_tsv(fullPath)
@@ -43,28 +46,40 @@ test <- data[, 9:ncol(data)] %>%
 test[which(is.na(test[,])), colnames(test)[colSums(is.na(test)) > 0]] = 0
 
 dateAndTime <- strptime(data$`Date-Time`[[1]], "%b %d %Y %H:%M:%S", tz="UTC")
-intervals <- data.frame(sizebin=colnames(test), volumepc=as.numeric(test[1,]), timestamp=rep(dateAndTime, ncol(test)))
+
+intervals <- data.frame(sizebin=factor(colnames(test), levels=colnames(test)), 
+                        volumepc=as.numeric(test[1,]),
+                        timestamp=rep(dateAndTime, ncol(test)))
+# intervals <- data.frame(sizebin=colnames(test), volumepc=as.numeric(test[1,]), timestamp=rep(dateAndTime, ncol(test)))
 
 for (row in 2:nrow(test)){
   dateAndTime <- strptime(data$`Date-Time`[[row]], "%b %d %Y %H:%M:%S", tz="UTC")
-  thisinterval <- data.frame(sizebin=colnames(test), volumepc=as.numeric(test[row,]), timestamp=rep(dateAndTime, ncol(test)))
+  thisinterval <- data.frame(sizebin=factor(colnames(test), levels=colnames(test)), 
+                             volumepc=as.numeric(test[row,]),
+                             timestamp=rep(dateAndTime, ncol(test)))
+  # thisinterval <- data.frame(sizebin=colnames(test), volumepc=as.numeric(test[row,]), timestamp=rep(dateAndTime, ncol(test)))
   intervals <- rbind(intervals, thisinterval)
 }
 
-# plottest <- ggplot(intervals, aes(x=sizebin, y=volumepc)) +
-#   geom_bar(stat='identity') +
-#   ylim(0, 100)
-# plottest
-
-plottest <- ggplot(intervals, aes(x=sizebin, y=volumepc)) +
+plottest <- ggplot(intervals, aes(x=sizebin, y=volumepc, fill=volumepc)) +
   geom_bar(stat='identity') +
-  ylim(0, (as.integer(max(volumepc)) + 1)) +
+  ylim(0, (as.integer(max(intervals$volumepc)) + 1)) +
+  theme_bw() +
+  theme(axis.text.x=element_text(size=rel(0.5), angle=90)) +
   transition_time(timestamp) +
   ease_aes('linear')
 
 
 animate(plottest, duration = 45.5, fps = 10, width = 1000, height = 500, renderer = gifski_renderer())
-anim_save("test-plot-animation.gif")
+anim_save("plotanimations/test-plot-animation.gif")
+
+timediff <- max(intervals$timestamp) - min(intervals$timestamp)
 
 
-barplot(height=as.numeric(test[5,]), names=colnames(test), col="green", border="pink")
+# barplot(height=as.numeric(test[1,]), names=colnames(test), col="green", border="pink")
+
+# dftest <- data.frame(size=factor(colnames(test), levels=colnames(test)), vals=as.numeric(test[1,]))
+# ggplot(dftest, aes(x=size, y=vals)) +
+#   theme_bw() +
+#   theme(axis.text.x=element_text(size=rel(0.5), angle=90)) +
+#   geom_bar(stat='identity')
